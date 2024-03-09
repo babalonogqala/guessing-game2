7c3037e28942cc50aeff5a8d40512cfa91c19cbd
Babalo <babalonogqala2003@gmail.com>
2024-03-09 14:57:33 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+const againBtn = document.querySelector('.btn again');
+const randomNumber = document.querySelector('.number');
+const number = Math.trunc(Math.random() * 20) - 1;
+console.log(number);
